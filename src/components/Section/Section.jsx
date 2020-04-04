1f4806994b9850f880a42cdc7ff267a052652321
Janette-cwk <chowwk@uw.edu>
2020-04-03 22:16:21 -0700

    New Section Display

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,147 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import { Grid,Row,Col } from "react-bootstrap";
+import { Card } from "components/Card/Card.jsx";
+import { FormInputs } from "components/FormInputs/FormInputs.jsx";
+import Button from "components/CustomButton/CustomButton.jsx";
+import PopUp from "components/PopUp/PopUp.jsx";
+import template1 from "assets/img/template1.JPG";
+import template2 from "assets/img/template2.JPG";
+import template3 from "assets/img/template3.JPG";
+import template4 from "assets/img/template4.JPG";
+import template5 from "assets/img/template5.JPG";
+import template6 from "assets/img/template6.JPG";
+const templates = [
+    {
+        imgSrc: template1,
+        label: "IMAGE WITH TEXT",
+    },
+    {
+        imgSrc: template2,
+        label: "FULLSCREEN VIDEO",
+    },
+    {
+        imgSrc: template3,
+        label: "VIDEO WITH CAPTION",
+    },
+    {
+        imgSrc: template4,
+        label: "FULLSCREEN IMAGE",
+    },
+    {
+        imgSrc: template5,
+        label: "TEXT",
+    },
+    {
+        imgSrc: template6,
+        label: "NUMBER LIST",
+    },
+]
+class CourseAdding extends Component {  
+    state = {
+        isOpen: false,
+        seen: false,
+        selectedOption: null,
+        selectedOptions: [],
+    };
+
+    handleChange = selectedOption => {
+        this.setState({selectedOption});
+    };
+    handleAddPage = () => {
+        const state = this.state;
+        if (state.selectedOption) {
+            this.setState({
+                selectedOptions: state.selectedOptions.concat([state.selectedOption]),
+                isOpen: false,
+                selectedOption : null,
+            });
+        }
+    }
+
+    togglePop = () => {
+        this.setState({
+            seen: !this.state.seen
+        });
+    };
+    render() {
+        const renderSelected = this.state.selectedOptions.map((t, index) =>(
+            <Col md={3} key={t.label}>
+                <p>{`Page ${index+1}`}</p>
+                <img className="templates-img" src={t.imgSrc}></img>
+                <label className="templates-label">{t.label}</label>
+            </Col>
+        ));
+        return (
+            <Row>
+                <Col md={12} className="new-card">
+                    <Card
+                        title={this.props.title}
+                        content={
+                            <div className='section'>
+                                <form>
+                                    <div>
+                                        <Button onClick={(e) => this.setState({ isOpen: true })} bsStyle="info" pullRight fill >
+                                            + New Page
+                                        </Button>
+                                        <PopUp isOpen={this.state.isOpen} onClose={(e) => this.setState({ isOpen: false })}>
+                                            <div className="course-popup">
+                                                <div className="course-title">
+                                                    <FormInputs
+                                                        ncols={["col-md-12"]}
+                                                        properties={[
+                                                            {
+                                                                label: "Title Name:",
+                                                                type: "text",
+                                                                bsClass: "form-control",
+                                                                placeholder: "Title Name"
+                                                            }
+                                                        ]}
+                                                    />
+                                                    <label>SELECT TEMPLATE</label>
+                                                    <hr />
+                                                </div>
+                                                <div className="course-templates">
+                                                    <Grid fluid>
+                                                        <Row>
+                                                            {templates.map(t => <Col md={2} key={t.label}>
+                                                                <a onClick={()=> this.handleChange(t)}>
+                                                                    <img  style={this.state.selectedOption === t ? {border:'1px solid #1A8A9A'}:null} src={t.imgSrc}></img>
+                                                                </a>
+                                                            <label>{t.label}</label>
+                                                            </Col>)}
+                                                        </Row>
+                                                    </Grid>
+                                                </div>
+                                                <hr />
+                                                    <Button bsStyle="info" pullRight fill onClick={this.handleAddPage}>
+                                                        + ADD PAGE
+                                                    </Button>
+                                            </div>
+                                        </PopUp>
+                                    </div>
+                                    <FormInputs
+                                        ncols={["col-md-10"]}
+                                        properties={[
+                                            {
+                                                label: "Section Name:",
+                                                type: "text",
+                                                bsClass: "form-control",
+                                                placeholder: "Section Name"
+                                            }
+                                        ]}
+                                    />
+                                </form>
+                                <hr />
+
+                                {/* if user select a template, the following shows up */}
+                                {renderSelected}
+                            </div>
+                        }
+                    />
+                </Col>
+            </Row >
+        )
+    }
+}
+export default CourseAdding;
--- src/views/CourseAdding.jsx
+++ src/views/CourseAdding.jsx
@@ -21,27 +21,34 @@ import template4 from "assets/img/template4.JPG";
 import template5 from "assets/img/template5.JPG";
 import template6 from "assets/img/template6.JPG";
 import template7 from "assets/img/template7.JPG";
+import Section from "components/Section/Section.jsx";
 
 
 //import avatar from "assets/img/faces/face-3.jpg";
 
 class CourseAdding extends Component {
-    state = {
-        seen: false
-    };
-
-    togglePop = () => {
-        this.setState({
-            seen: !this.state.seen
-        });
-    };
+    constructor() {
+        super();
+        // this.state = {showSection: false }
+        this.state = {showSection: 1}
+    }
+    _showSection = () => {
+        // this.setState({
+        //     showSection: true
+        // });
+        this.setState({showSection: this.state.showSection + 1})
+    }
     render() {
+        const sections = [];
+        for (var i=0; i< this.state.showSection; i++){
+            sections.push(<Section title={`Section ${i+1}`} key={i}/>)
+        }
         return (
-            <div className="content">
+            <div className="content add-content">
                 <Grid fluid>
                     <Row>
                         <form>
-                            <Button bsStyle="info" pullRight fill >
+                            <Button onClick={this._showSection} bsStyle="info" pullRight fill >
                                 + New Section
                             </Button>
                             <FormInputs
@@ -58,111 +65,7 @@ class CourseAdding extends Component {
                             />
                         </form>
                     </Row>
-                    <Row>
-                        <Col md={12} className="new-card">
-                            <Card
-                                title="Section 1"
-                                content={
-                                    <div className='section'>
-                                        <form>
-                                            <div>
-                                                <Button onClick={(e) => this.setState({ isOpen: true })} bsStyle="info" pullRight fill >
-                                                    + New Page
-                                                </Button>
-                                                <PopUp isOpen={this.state.isOpen} onClose={(e) => this.setState({ isOpen: false })}>
-                                                    <div className="course-popup">
-                                                        <div className="course-title">
-                                                            <FormInputs
-                                                                ncols={["col-md-12"]}
-                                                                properties={[
-                                                                    {
-                                                                        label: "Title Name:",
-                                                                        type: "text",
-                                                                        bsClass: "form-control",
-                                                                        placeholder: "Title Name"
-                                                                    }
-                                                                ]}
-                                                            />
-                                                            <label>SELECT TEMPLATE</label>
-                                                            <hr />
-                                                        </div>
-                                                        <div className="course-templates">
-                                                            <Grid fluid>
-                                                                <Row>
-                                                                    <Col md={2}>
-
-                                                                        <a><img src={template1}></img></a>
-                                                                        <label>IMAGE WITH TEXT</label>
-
-                                                                    </Col>
-
-                                                                    <Col md={2}>
-
-                                                                        <a><img src={template2}></img></a>
-                                                                        <label>FULLSCREEN VIDEO</label>
-
-                                                                    </Col>
-                                                                    <Col md={2}>
-
-                                                                        <a><img src={template3}></img></a>
-                                                                        <label>VIDEO WITH CAPTION</label>
-
-                                                                    </Col>
-                                                                    <Col md={2}>
-
-                                                                        <a><img src={template4}></img></a>
-                                                                        <label>FULLSCREEN IMAGE</label>
-
-                                                                    </Col>
-
-                                                                    <Col md={2}>
-
-                                                                        <a><img src={template5}></img></a>
-                                                                        <label>TEXT</label>
-
-                                                                    </Col>
-                                                                    <Col md={2}>
-
-                                                                        <a><img src={template6}></img></a>
-                                                                        <label>NUMBER LIST</label>
-
-                                                                    </Col>
-                                                                </Row>
-                                                            </Grid>
-                                                        </div>
-                                                        <hr />
-                                                        <Link to='/admin/addcourse'>
-                                                            <Button bsStyle="info" pullRight fill >
-                                                                + ADD PAGE
-                                                            </Button>
-                                                        </Link>
-                                                    </div>
-                                                </PopUp>
-                                            </div>
-                                            <FormInputs
-                                                ncols={["col-md-10"]}
-                                                properties={[
-                                                    {
-                                                        label: "Section Name:",
-                                                        type: "text",
-                                                        bsClass: "form-control",
-                                                        placeholder: "Section Name",
-                                                        defaultValue: "Introduction"
-                                                    }
-                                                ]}
-                                            />
-                                        </form>
-                                        <hr />
-
-                                        {/* if user select a template, the following shows up */}
-                                        <p>Page 1</p>
-                                        <img src={template1} alt="..." />
-                                        <p>Images with text</p>
-                                    </div>
-                                }
-                            />
-                        </Col>
-                    </Row>
+                    {sections}
                 </Grid>
                 <Link to='/admin/courseoutline'>
                     <Button bsStyle="info" pullRight fill type="submit" >
